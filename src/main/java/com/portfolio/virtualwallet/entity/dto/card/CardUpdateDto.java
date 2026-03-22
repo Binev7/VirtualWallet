e389d36f05aeaf6b631c78b3c11a5076fe916670
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-22 20:27:44 +0200

    feat: add CardUpdateDto

---
+++ src/main/java/com/portfolio/virtualwallet/entity/dto/card/CardUpdateDto.java
@@ -0,0 +1,24 @@
+package com.portfolio.virtualwallet.entity.dto.card;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+import static com.portfolio.virtualwallet.entity.dto.constants.CardValidationMessages.*;
+import static com.portfolio.virtualwallet.entity.dto.constants.CardValidationMessages.CARD_HOLDER_SIZE;
+
+@Data
+public class CardUpdateDto {
+
+    @NotNull(message = EXPIRATION_DATE_NOT_NULL)
+    @Future(message = EXPIRATION_DATE_FUTURE)
+    private LocalDate expirationDate;
+
+    @NotBlank(message = CARD_HOLDER_NOT_BLANK)
+    @Size(min = 2, max = 30, message = CARD_HOLDER_SIZE)
+    private String cardHolder;
+}
