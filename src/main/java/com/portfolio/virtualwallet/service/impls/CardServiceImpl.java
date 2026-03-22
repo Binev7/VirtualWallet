e35c060dedce59a102efa3b18c1fde27b21e8639
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-22 20:28:51 +0200

    feat: add new methods in CardService

--- src/main/java/com/portfolio/virtualwallet/service/impls/CardServiceImpl.java
+++ src/main/java/com/portfolio/virtualwallet/service/impls/CardServiceImpl.java
@@ -4,15 +4,21 @@ import com.portfolio.virtualwallet.entity.Card;
 import com.portfolio.virtualwallet.entity.User;
 import com.portfolio.virtualwallet.entity.dto.card.CardCreateDto;
 import com.portfolio.virtualwallet.entity.dto.card.CardResponseDto;
+import com.portfolio.virtualwallet.entity.dto.card.CardUpdateDto;
 import com.portfolio.virtualwallet.exception.DuplicateEntityException;
 import com.portfolio.virtualwallet.exception.EntityNotFoundException;
+import com.portfolio.virtualwallet.exception.ExceptionMessages;
 import com.portfolio.virtualwallet.mapper.CardMapper;
 import com.portfolio.virtualwallet.repository.CardRepository;
 import com.portfolio.virtualwallet.repository.UserRepository;
 import com.portfolio.virtualwallet.service.interfaces.CardService;
+import com.portfolio.virtualwallet.utils.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.portfolio.virtualwallet.exception.ExceptionMessages.User.*;
 import static com.portfolio.virtualwallet.exception.ExceptionMessages.Card.*;
@@ -28,10 +34,10 @@ public class CardServiceImpl implements CardService {
     @Override
     public CardResponseDto addCard(CardCreateDto request) {
 
-        String currentUserEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        String currentUsername = SecurityUtils.getCurrentUsername();
 
-        User user = userRepository.findByEmail(currentUserEmail)
-                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
+        User user = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new EntityNotFoundException(NOT_FOUND));
 
         if (cardRepository.existsByCardNumber(request.getCardNumber())) {
             throw new DuplicateEntityException(ALREADY_EXISTS);
@@ -43,5 +49,46 @@ public class CardServiceImpl implements CardService {
         Card savedCard = cardRepository.save(card);
 
         return cardMapper.toDto(savedCard);
+    }
+
+    @Override
+    public List<CardResponseDto> getAllMyCards() {
+        String username = SecurityUtils.getCurrentUsername();
+
+        return cardRepository.findByUserUsername(username)
+                .stream()
+                .map(cardMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CardResponseDto getCardById(Long id) {
+        Card card = getCardIfOwned(id);
+        return cardMapper.toDto(card);
+    }
+
+    @Override
+    public CardResponseDto updateCard(Long id, CardUpdateDto request) {
+        Card card = getCardIfOwned(id);
+
+        card.setExpirationDate(request.getExpirationDate());
+        card.setCardHolder(request.getCardHolder());
+
+        Card updatedCard = cardRepository.save(card);
+        return cardMapper.toDto(updatedCard);
+    }
+
+    @Override
+    public void deleteCard(Long id) {
+        Card card = getCardIfOwned(id);
+        cardRepository.delete(card);
+    }
+
+    @Override
+    public Card getCardIfOwned(Long cardId) {
+        String username = SecurityUtils.getCurrentUsername();
+
+        return cardRepository.findByIdAndUserUsername(cardId, username)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessages.Card.NOT_FOUND));
     }
 }
--- src/main/java/com/portfolio/virtualwallet/service/interfaces/CardService.java
+++ src/main/java/com/portfolio/virtualwallet/service/interfaces/CardService.java
@@ -1,8 +1,22 @@
 package com.portfolio.virtualwallet.service.interfaces;
 
+import com.portfolio.virtualwallet.entity.Card;
 import com.portfolio.virtualwallet.entity.dto.card.CardCreateDto;
 import com.portfolio.virtualwallet.entity.dto.card.CardResponseDto;
+import com.portfolio.virtualwallet.entity.dto.card.CardUpdateDto;
+
+import java.util.List;
 
 public interface CardService {
     CardResponseDto addCard(CardCreateDto request);
+
+    List<CardResponseDto> getAllMyCards();
+
+    CardResponseDto getCardById(Long id);
+
+    CardResponseDto updateCard(Long id, CardUpdateDto request);
+
+    void deleteCard(Long id);
+
+    Card getCardIfOwned(Long cardId);
 }
