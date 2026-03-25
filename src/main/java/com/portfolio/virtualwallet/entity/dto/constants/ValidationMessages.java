2e2241aa4b6bb24290e7a7bc21fc839c0cc7b54e
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-25 15:40:33 +0200

    feat: add validation messages for wallet and add comments for better readability

--- src/main/java/com/portfolio/virtualwallet/entity/dto/constants/ValidationMessages.java
+++ src/main/java/com/portfolio/virtualwallet/entity/dto/constants/ValidationMessages.java
@@ -5,15 +5,23 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ValidationMessages {
 
+    // --- USERNAME VALIDATION ---
     public final String USERNAME_NOT_BLANK = "Username cannot be empty";
     public final String USERNAME_SIZE = "Username must be between 2 and 20 symbols.";
 
+    // --- PASSWORD VALIDATION ---
     public final String PASSWORD_NOT_BLANK = "Password cannot be empty";
     public final String PASSWORD_PATTERN = "Password must be at least 8 characters and contain a capital letter, a digit, and a special symbol (+, -, *, &, ^).";
 
+    // --- EMAIL VALIDATION ---
     public final String EMAIL_NOT_BLANK = "Email cannot be empty";
     public final String EMAIL_INVALID = "Email must be valid.";
 
+    // --- PHONE VALIDATION ---
     public final String PHONE_NOT_BLANK = "Phone number cannot be empty";
     public final String PHONE_PATTERN = "Phone number must be exactly 10 digits.";
+
+    // --- WALLET VALIDATION ---
+    public final String WALLET_NAME_NOT_BLANK = "Wallet name cannot be empty.";
+    public final String WALLET_NAME_SIZE = "Wallet name must be between 2 and 30 characters.";
 }
