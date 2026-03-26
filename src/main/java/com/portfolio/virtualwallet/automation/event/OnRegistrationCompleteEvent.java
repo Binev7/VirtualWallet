82f516d93c842313bbcb23148faebb5b0d7235f7
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-26 12:29:42 +0200

    feat: add Registration event

---
+++ src/main/java/com/portfolio/virtualwallet/automation/event/OnRegistrationCompleteEvent.java
@@ -0,0 +1,12 @@
+package com.portfolio.virtualwallet.automation.event;
+
+import com.portfolio.virtualwallet.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OnRegistrationCompleteEvent {
+    private final User user;
+    private final String appUrl;
+}
