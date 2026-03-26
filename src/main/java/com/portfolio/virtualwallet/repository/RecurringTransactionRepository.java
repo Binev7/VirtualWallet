d2964ef8d7833e7446ec1e13c0fe408bf8bff29d
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-26 17:44:33 +0200

    feat: add transaction repositories

---
+++ src/main/java/com/portfolio/virtualwallet/repository/RecurringTransactionRepository.java
@@ -0,0 +1,20 @@
+package com.portfolio.virtualwallet.repository;
+
+import com.portfolio.virtualwallet.entity.RecurringTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface RecurringTransactionRepository extends JpaRepository<RecurringTransaction, Long> {
+
+    @Query("SELECT rt FROM RecurringTransaction rt WHERE rt.isActive = true AND rt.nextExecutionTime <= :currentTime")
+    List<RecurringTransaction> findAllDueTransactions(@Param("currentTime") LocalDateTime currentTime);
+
+    @Query("SELECT rt FROM RecurringTransaction rt WHERE rt.senderWallet.id = :walletId AND rt.isActive = true")
+    List<RecurringTransaction> findActiveSubscriptionsByWallet(@Param("walletId") Long walletId);
+}
