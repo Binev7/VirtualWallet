af304a3d3a385f5cf7f382b8d099587c6595b4af
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-30 14:47:11 +0300

    feat: add new verifyOtp method

--- src/main/java/com/portfolio/virtualwallet/service/interfaces/TransactionService.java
+++ src/main/java/com/portfolio/virtualwallet/service/interfaces/TransactionService.java
@@ -1,9 +1,11 @@
 package com.portfolio.virtualwallet.service.interfaces;
 
 import com.portfolio.virtualwallet.entity.User;
+import com.portfolio.virtualwallet.entity.dto.transaction.OtpVerificationRequestDto;
 import com.portfolio.virtualwallet.entity.dto.transaction.TransactionResponseDto;
 import com.portfolio.virtualwallet.entity.dto.transaction.TransferRequestDto;
 
 public interface TransactionService {
     TransactionResponseDto transfer(User currentUser, TransferRequestDto request);
+    TransactionResponseDto verifyOtp(User currentUser, OtpVerificationRequestDto request);
 }
