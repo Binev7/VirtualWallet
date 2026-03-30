cc7386da0b2d18fc84a1f54f7cd6514ecee73593
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-30 11:29:24 +0300

    refactor: email send logic

--- src/main/java/com/portfolio/virtualwallet/service/impls/EmailServiceImpl.java
+++ src/main/java/com/portfolio/virtualwallet/service/impls/EmailServiceImpl.java
@@ -5,7 +5,6 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import static com.portfolio.virtualwallet.utils.AppConstants.Email.*;
+import java.util.Map;
 import static com.portfolio.virtualwallet.utils.AppConstants.Logging.*;
 
 @Slf4j
@@ -29,25 +28,22 @@ public class EmailServiceImpl implements EmailService {
 
     @Async
     @Override
-    public void sendVerificationEmail(String to, String confirmationUrl) {
+    public void sendHtmlEmail(String to, String subject, String templateName, Map<String, Object> variables) {
         try {
             Context context = new Context();
-            context.setVariable(URL_VARIABLE, confirmationUrl);
+            context.setVariables(variables);
 
-            String htmlContent = templateEngine.process(VERIFICATION_TEMPLATE, context);
+            String htmlContent = templateEngine.process(templateName, context);
 
             MimeMessage message = mailSender.createMimeMessage();
-
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
             helper.setFrom(senderEmail);
             helper.setTo(to);
-            helper.setSubject(VERIFICATION_SUBJECT);
+            helper.setSubject(subject);
             helper.setText(htmlContent, true);
 
             mailSender.send(message);
-            log.info(EMAIL_SEND_SUCCESS, to);
-
         } catch (Exception e) {
             log.error(EMAIL_SEND_ERROR, to, e);
         }
--- src/main/java/com/portfolio/virtualwallet/service/interfaces/EmailService.java
+++ src/main/java/com/portfolio/virtualwallet/service/interfaces/EmailService.java
@@ -1,5 +1,7 @@
 package com.portfolio.virtualwallet.service.interfaces;
 
+import java.util.Map;
+
 public interface EmailService {
-    void sendVerificationEmail(String to, String confirmationUrl);
+    void sendHtmlEmail(String to, String subject, String templateName, Map<String, Object> variables);
 }
