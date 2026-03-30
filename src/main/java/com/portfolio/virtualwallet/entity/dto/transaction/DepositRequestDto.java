63e68548cb9103694457195d1a69ab49389bc2cc
Binev7 <georgi_binev.gogo@abv.bg>
2026-03-30 17:09:47 +0300

    feat: add deposit, withdraw and bank response dto's

---
+++ src/main/java/com/portfolio/virtualwallet/entity/dto/transaction/DepositRequestDto.java
@@ -0,0 +1,29 @@
+package com.portfolio.virtualwallet.entity.dto.transaction;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+import static com.portfolio.virtualwallet.entity.dto.constants.ValidationMessages.*;
+
+@Data
+public class DepositRequestDto {
+
+    @NotNull(message = WALLET_ID_NOT_NULL)
+    private Long walletId;
+
+    @NotNull(message = CARD_ID_NOT_NULL)
+    private Long cardId;
+
+    @NotNull(message = AMOUNT_NOT_NULL)
+    @DecimalMin(value = "0.01", message = AMOUNT_MIN_VALUE)
+    private BigDecimal amount;
+
+    @NotBlank(message = CVV_NOT_BLANK)
+    @Pattern(regexp = "^[0-9]{3}$", message = CVV_PATTERN)
+    private String cvv;
+}
